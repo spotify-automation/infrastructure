58b7722d64e653ff4afbc7a0356d4934c7a56e3d
Aaron Mamparo <aaronmamparo@gmail.com>
2020-09-23 20:23:46 -0500

    first commit

---
+++ aws/stack.py
@@ -0,0 +1,60 @@
+from aws_cdk.aws_iam import Role, PolicyDocument, PolicyStatement, Effect, ServicePrincipal
+from aws_cdk.core import Stack, Construct, App, Duration
+import os
+import boto3
+
+
+class MainStack(Stack):
+    def __init__(self, scope: Construct, _id: str, **kwargs) -> None:
+        super().__init__(scope, _id, **kwargs)
+        lambda_role = Role(
+            self,
+            'LambdaRole',
+            assumed_by=ServicePrincipal('lambda.amazonaws.com'),
+            inline_policies={
+                's3': PolicyDocument(
+                    statements=[
+                        PolicyStatement(
+                            effect=Effect.ALLOW,
+                            actions=[
+                                's3:ListBucket',
+                                's3:PutObject',
+                                's3:GetObject',
+                                's3:ListObjects'
+                            ],
+                            resources=[
+                                'arn:aws:s3:::*'
+                            ]
+                        )
+                    ]
+                ),
+                'lambda': PolicyDocument(
+                    statements=[
+                        PolicyStatement(
+                            effect=Effect.ALLOW,
+                            actions=['lambda:InvokeFunction'],
+                            resources=['arn:aws:lambda:*:*:function:*']
+                        )
+                    ]
+                ),
+                'logs': PolicyDocument(
+                    statements=[
+                        PolicyStatement(
+                            effect=Effect.ALLOW,
+                            actions=[
+                                'logs:CreateLogGroup',
+                                'logs:CreateLogStream',
+                                'logs:PutLogEvents'
+                            ],
+                            resources=['*']
+                        )
+                    ]
+                )
+            }
+        )
+
+
+if __name__ == '__main__':
+    app = App()
+    MainStack(app, 'yahoo-fantasy-football-scrape-pipeline')
+    app.synth()
