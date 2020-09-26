4f85c15b25877a748996f5c7864e7eba301b3784
Aaron Mamparo <aaronmamparo@gmail.com>
2020-09-26 16:37:58 -0500

    python function resource

---
+++ src/infrastructure/cdk/resources.py
@@ -0,0 +1,20 @@
+from typing import Dict
+
+from aws_cdk.aws_lambda import Function, Runtime, Code
+from aws_cdk.core import Construct, Duration
+
+from .helpers import get_stack_output
+
+
+def create_python_function(scope: Construct, _id: str, handler_path: str, environment: Dict[str, str]) -> Function:
+    return Function(
+        scope,
+        _id,
+        runtime=Runtime.PYTHON_3_7,
+        code=Code.from_asset('.build', exclude=['boto3']),
+        handler=handler_path,
+        role=get_stack_output('yahoo-fantasy-football-infrastructure', 'LambdaRoleArn'),
+        environment=environment or {},
+        timeout=Duration.minutes(15),
+        memory_size=256
+    )
