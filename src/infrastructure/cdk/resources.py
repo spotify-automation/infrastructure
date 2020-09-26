e877c4f47dd41247ac69c5410416a20f8ffd357c
Aaron Mamparo <aaronmamparo@gmail.com>
2020-09-26 17:31:50 -0500

    get role from arn

--- src/infrastructure/cdk/resources.py
+++ src/infrastructure/cdk/resources.py
@@ -1,5 +1,6 @@
 from typing import Dict
 
+from aws_cdk.aws_iam import Role
 from aws_cdk.aws_lambda import Function, Runtime, Code
 from aws_cdk.core import Construct, Duration
 
@@ -13,7 +14,7 @@ def create_python_function(scope: Construct, _id: str, handler_path: str, enviro
         runtime=Runtime.PYTHON_3_7,
         code=Code.from_asset('.build', exclude=['boto3']),
         handler=handler_path,
-        role=get_stack_output('yahoo-fantasy-football-infrastructure', 'LambdaRoleArn'),
+        role=Role.from_role_arn(get_stack_output('yahoo-fantasy-football-infrastructure', 'LambdaRoleArn')),
         environment=environment or {},
         timeout=Duration.minutes(15),
         memory_size=256
