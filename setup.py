cc58df2dfea839af8ab41add294c73a1ee8a9a80
bumpversion <bumpversion@circleci.com>
2020-09-26 18:00:05 +0000

    "Bump version: b'0.0.7\n' → 0.0.8 [skip ci]"

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='infrastructure',
-    version='0.0.7',
+    version='0.0.8',
     author='Aaron Mamparo',
     author_email='aaronmamparo@gmail.com',
     description='Shared infrastructure helpers for the Yahoo Fantasy Football project',
