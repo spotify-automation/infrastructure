5383c5b011f3cfb9d6daf4e09fdb35d514ae7573
Aaron Mamparo <aaronmamparo@gmail.com>
2020-09-26 19:23:18 -0500

    fix requirements file writing

--- src/infrastructure/build_lambda_deployment_package.py
+++ src/infrastructure/build_lambda_deployment_package.py
@@ -3,14 +3,18 @@ import shlex
 
 
 def __run_command(argstr: str) -> int:
-    return subprocess.run(shlex.split(argstr.strip(), posix=False), shell=True).returncode
+    return subprocess.run(shlex.split(argstr.strip(), posix=False)).returncode
 
 
 def main():
     __run_command('rm -rf .build')
     __run_command('mkdir .build')
     __run_command('cp -r src .build')
-    __run_command('pipenv run pip freeze > .build/requirements.txt')
+    requirements = str(subprocess.check_output('pipenv run pip freeze'.split()), 'utf-8')
+    print('requirements:')
+    print(requirements)
+    with open('.build/requirements.txt', 'w+') as requirements_file:
+        requirements_file.writelines(requirements.splitlines())
     __run_command('pip install -r .build/requirements.txt -t .build --compile')
 
 
